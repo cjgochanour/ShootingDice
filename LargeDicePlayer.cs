3dc189012195b95817b61b1a6d01603347287695
cjgochanour <cjgochanour@gmail.com>
2022-03-11 11:41:21 -0600

    modified a comment on LargeDicePlayer.cs

--- LargeDicePlayer.cs
+++ LargeDicePlayer.cs
@@ -1,6 +1,6 @@
 namespace ShootingDice
 {
-    // A Player who brings a large die to the game
+    // A Player who brings a large die to the game, inherits from PLayer - constructor sets DiceSize to 20 by default
     public class LargeDicePlayer : Player
     {
         public LargeDicePlayer()
